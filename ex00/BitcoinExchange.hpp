b2d9cd792f62200d7706597439f97f50d8ad4e3b
ZXyoukai <martinhocarneiros@gmail.com>
2025-10-16 15:51:19 +0100

    Refactor Date class for improved parsing and error handling; update Makefile formatting and restore input.txt with sample data

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dgermano <dgermano@student.42.fr>          #+#  +:+       +#+        */
+/*   By: dgermano <dgermano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-10-16 12:51:12 by dgermano          #+#    #+#             */
-/*   Updated: 2025-10-16 12:51:12 by dgermano         ###   ########.fr       */
+/*   Created: 2025/10/16 12:51:12 by dgermano          #+#    #+#             */
+/*   Updated: 2025/10/16 15:51:19 by dgermano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,25 +49,37 @@ Date::Date(const std::string  & line): y(0), d(0), m(0), value(0.0f)
     try
     {
         std::istringstream ss(line);
+        std::string tmp;
+
+        for (size_t i = 0; i < 4; i)
+        {
+            if((ss >> tmp); tmp.compare("-") )
+            i++;
+        }
+        
         ss >> this->y;
         ss >> this->m;
         ss >> this->d;
         ss >> this->value;
+
+        std::cout << y << m << d << value << std::endl;
+        exit(0);
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
         return ;
     }
-    if (y > 2025 || y < 0)
-        std::cerr << "Error invalid year" << std::endl;
-    if (m > 12 || m < 0)
-        std::cerr << "Error invalid moth" << std::endl;
-    if (d > 31 || d < 0)
-        std::cerr << "Error invalid day" << std::endl;
-    if (value < 0 || value > 1000)
-        std::cerr << "Error invalid value" << std::endl;
+    // if (y > 2025 || y < 0)
+    //     std::cerr << "Error invalid year" << std::endl;
+    // else if (m > 12 || m < 0)
+    //     std::cerr << "Error invalid moth" << std::endl;
+    // else if (d > 31 || d < 0)
+    //     std::cerr << "Error invalid day" << std::endl;
+    // else if (value < 0 || value > 1000)
+    //     std::cerr << "Error invalid value" << std::endl;
 }
+Date::~Date(){};
 
 BitcoinExchange::BitcoinExchange(const std::string path)
 {
@@ -92,3 +104,5 @@ BitcoinExchange::BitcoinExchange(const std::string path)
     }
     
 }
+
+BitcoinExchange::~BitcoinExchange(){};
