75cca6b53894b4806aa8ac0cd3e33047138aa236
ZXyoukai <martinhocarneiros@gmail.com>
2025-10-16 14:55:40 +0100

    Implement BitcoinExchange class and Date parsing functionality
    - Added BitcoinExchange class to handle Bitcoin data from a file.
    - Created Date class to parse and validate date and value entries.
    - Implemented error handling for invalid date formats and values.
    - Added main function to initialize BitcoinExchange with a file path.
    - Included a sample input file for testing purposes.

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgermano <dgermano@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-10-16 12:51:12 by dgermano          #+#    #+#             */
+/*   Updated: 2025-10-16 12:51:12 by dgermano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <stdexcept>
+
+class Date
+{
+    private:
+        int d;
+        int m;
+        int y;
+        float value;
+    public:
+        Date( const std::string & line);
+        // Date( const Date & copy);
+        // Date & operator = (const Date & obj);
+        ~Date();
+};
+
+class BitcoinExchange
+{
+    private:
+        std::vector<Date> dts;
+    public:
+        BitcoinExchange(const std::string path);
+        // BitcoinExchange(const BitcoinExchange & copy);
+        // BitcoinExchange & operator = (const BitcoinExchange & obj);
+        ~BitcoinExchange();
+};
+
+Date::Date(const std::string  & line): y(0), d(0), m(0), value(0.0f)
+{
+    try
+    {
+        std::istringstream ss(line);
+        ss >> this->y;
+        ss >> this->m;
+        ss >> this->d;
+        ss >> this->value;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return ;
+    }
+    if (y > 2025 || y < 0)
+        std::cerr << "Error invalid year" << std::endl;
+    if (m > 12 || m < 0)
+        std::cerr << "Error invalid moth" << std::endl;
+    if (d > 31 || d < 0)
+        std::cerr << "Error invalid day" << std::endl;
+    if (value < 0 || value > 1000)
+        std::cerr << "Error invalid value" << std::endl;
+}
+
+BitcoinExchange::BitcoinExchange(const std::string path)
+{
+    std::fstream iFile(path);
+
+    if(iFile.fail())
+        std::cerr << "Error opening file" << std::endl;
+    while (!iFile.eof())
+    {
+        std::string line;
+        std::getline(iFile, line);
+        try
+        {
+            Date dt(line);
+            dts.push_back(dt);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        
+    }
+    
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgermano <dgermano@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-10-16 13:54:20 by dgermano          #+#    #+#             */
+/*   Updated: 2025-10-16 13:54:20 by dgermano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        std::cerr << "Error: could not open file." << std::endl;
+        return 1;
+    }
+    BitcoinExchange btc(av[1]);
+    return 0;
+
+};
